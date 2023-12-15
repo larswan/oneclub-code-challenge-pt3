36bfdc02d068c36ac66b248007ffde4c90527e8e
Larson Collier <collierlarson@gmail.com>
2023-12-15 11:54:00 -0500

    Added form input and validateName function. Need to change handling of formData and validation.

--- src/FormSteps/Step1.jsx
+++ src/FormSteps/Step1.jsx
@@ -1,9 +1,26 @@
-const Step1 = () => {
-    
+const Step1 = ({ changeStep, formData, setFormData }) => {
+
+    const validateName = (inputName) => {
+        const trimmedString = inputName.trim()
+        if (trimmedString ==="") {
+            window.alert("First name cannot be empty!")
+            return false
+        }
+        else return trimmedString 
+    }
+
     return(
         <div className="stepContainer">
-
-            Step 1
+            <h1>Step 1</h1>
+            <label>First Name: </label>
+            <input 
+            type="text" 
+            name="First Name" 
+            placeholder="enter first name" 
+            id="firstName" 
+            />
+            <button onClick={e=>changeStep(e, -1)}>Previous Step</button>
+            <button onClick={e => changeStep(e, 1)}>Next Step</button>
         </div>
     )
 }
--- src/WizardForm.jsx
+++ src/WizardForm.jsx
@@ -1,4 +1,31 @@
-import { createBrowserRouter, RouterProvider, Route, useNavigate } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react'
+import Step1 from './FormSteps/Step1'
 
 
+
+const WizardForm = () => {
+    const [formData, setFormData] = useState({
+        firstName: "Michael",
+        lastName: "",
+        age,
+        email: "",
+    })
+
+    const onNext = (e, stepChange) => {
+        e.preventDefault()
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+
+        // change step using stepChange
+    }
+
+    return(
+        <div className="formContainer">
+            <Step1 formData={formData} setFormData={setFormData} />
+            
+        </div>
+    )
+}
