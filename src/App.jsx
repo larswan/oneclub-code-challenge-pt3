28909d294261899f236217744a98c0d74b9bbdd2
Larson Collier <collierlarson@gmail.com>
2023-12-15 12:48:11 -0500

    react router set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import { useState } from 'react'
-import WizardForm from './WizardForm'
+import React from 'react';
+import { useState } from 'react';
+import { Route, Routes, BrowserRouter } from "react-router-dom"
+import { useNavigate } from 'react-router-dom';
+import WizardForm from './WizardForm';
+import Step1 from './FormSteps/Step1';
+import Step2 from './FormSteps/Step2';
 
 function App() {
 
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    age: 25,
+    email: "",
+  })
+
+  const onNext = (e, stepChange) => {
+    e.preventDefault()
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+
+    // change step using stepChange
+  }
+
+
   return (
     <div className="app">
-      <WizardForm />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WizardForm />} />
+          <Route path="/step1" element={<Step1  />} />
+        </Routes>
+      </ BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/WizardForm.jsx
+++ src/WizardForm.jsx
@@ -1,30 +1,13 @@
-import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react'
-import Step1 from './FormSteps/Step1'
-import Step2 from './FormSteps/Step2';
+import { useNavigate } from 'react-router-dom'
 
 const WizardForm = () => {
-    const [formData, setFormData] = useState({
-        firstName: "",
-        lastName: "",
-        age: 25,
-        email: "",
-    })
-
-    const onNext = (e, stepChange) => {
-        e.preventDefault()
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        })
-
-        // change step using stepChange
-    }
+    const navigate = useNavigate()
 
     return(
-        <div className="formContainer">
-            <Step1 formData={formData} setFormData={setFormData} />
-            
+        <div>
+            <h1>Wizard Form</h1> 
+            <button onClick={()=>navigate("/Step1")}>Start Form</button>
         </div>
     )
 }
